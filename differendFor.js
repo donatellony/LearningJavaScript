d31695219ba7003a878e6a8892f9b80ccc1bba49
donatellony <donatellony4736@gmail.com>
2020-08-02 14:20:24 +0300

    Some strange JS things and forEach

---
+++ differendFor.js
@@ -0,0 +1,26 @@
+let codes = {
+    "+7": "Россия",
+    "+38": "Украина",
+    "+1": "США",
+    mycodes:{
+        bg: 'Black',
+        fg: 'Red'
+    },
+    makeTest: function(){
+        console.log("test");
+    }
+  };
+
+  const {bg, fg} = codes.mycodes; // деструктуризация
+  codes.makeTest();
+  
+  for (let code in codes) { //также можно сделать (for of, но оно не работает с объектами)
+    let value = codes[code];
+    code = +code; // ..если нам нужно именно число, преобразуем: "+7" -> 7
+  
+    alert( code + ": " + value ); // 7, 38, 1 во всех браузерах
+  }
+  //FOREACH
+
+
+  // Урок 1.20
