3516741612a8bfdca3d62ecfce427fe92a3ec94f
donatellony <donatellony4736@gmail.com>
2020-09-03 14:40:41 +0300

    Timer works now

--- Food/js/myscript.js
+++ Food/js/myscript.js
@@ -84,5 +84,55 @@ window.addEventListener('DOMContentLoaded', ()=>{
     totalSlideNum.textContent = "0" + totalSlide;
 
     //TIME
+
+    const deadline = '2020-09-07';
     
+    function getTimeRemaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+              minutes = Math.floor((t / (1000 * 60) % 60)),
+              seconds = Math.floor((t / 1000) % 60);
+        
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function addZeroIfNeeded(num){
+        if(num>=0 && num<10){
+            return "0"+num;
+        }else{
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+          seconds = timer.querySelector('#seconds'),
+          minutes = timer.querySelector('#minutes'),
+          hours = timer.querySelector('#hours'),
+          days = timer.querySelector('#days'),
+          timeInterval = setInterval(updateClock, 1000);
+        
+        updateClock();
+        
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+            days.textContent = addZeroIfNeeded(t.days);
+            hours.textContent = addZeroIfNeeded(t.hours);
+            minutes.textContent = addZeroIfNeeded(t.minutes);
+            seconds.textContent = addZeroIfNeeded(t.seconds);
+
+            if(t.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    
+    setClock('.timer', deadline);
 });
