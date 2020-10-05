798db072d3daed570b4186f39fca282e64af3a2c
donatellony <donatellony4736@gmail.com>
2020-10-05 13:01:21 +0300

    Added REST operator to the class MenuCard

--- Food/js/myscript.js
+++ Food/js/myscript.js
@@ -197,19 +197,25 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const menuItems = document.querySelectorAll('.menu__item');
 
     class MenuCard{
-        constructor(imgSrc, alt, name, descr, price, parentSelector){
+        constructor(imgSrc, alt, name, descr, price, parentSelector, ...classes){
             this.imgSrc = imgSrc;
             this.alt = alt;
             this.name = name;
             this.descr = descr;
             this.parent = document.querySelector(parentSelector);
             this.price = price;
+            this.classes = classes;
         }
         
         render() {
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+            this.classes.forEach(className => element.classList.add(className));
+            }
             element.innerHTML = `
-            <div class="menu__item">
             <img src="img/tabs/vegy.jpg" alt="${this.alt}">
             <h3 class="menu__item-subtitle">${this.name}</h3>
             <div class="menu__item-descr">${this.descr}</div>
@@ -218,11 +224,18 @@ window.addEventListener('DOMContentLoaded', ()=>{
                 <div class="menu__item-cost">Цена:</div>
                 <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
             </div>
-        </div>
         `;
         this.parent.append(element);
         }
     }
-    new MenuCard("img/tabs/vegy.jpg","vegy","JS Added","Test menu",2000, '.menu .container').render();
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "JS Added",
+        "Test menu",
+        2000,
+         '.menu .container',
+         'menu__item'
+         ).render();
 
 }); 
